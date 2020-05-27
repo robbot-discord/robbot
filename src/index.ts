aa51144bf9049260e4615c4912b54a9a09e07ce6
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-05-27 03:40:38 -0400

    Create first version of RobBot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello world!")
+import { createClient, createDefaultConfiguration } from "@robbot/robbot-core"
+
+const apiToken = process.env.DISCORD_API_TOKEN as string
+
+const configuration = createDefaultConfiguration(apiToken)
+const robBot = createClient(configuration)
+
+robBot.run()
