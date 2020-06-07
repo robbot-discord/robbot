1f94ac1e8d48531780b8266d507b38c72cbbbec6
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-06-07 14:57:54 -0400

    Update robbot-core to latest version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
-import { createClient, createDefaultConfiguration } from "@robbot/robbot-core"
+import {
+  createClient,
+  createDefaultConfiguration,
+  LogLevel,
+  runClient,
+} from "@robbot/robbot-core"
+import {
+  createLoggingLevelFilter,
+  LogLevelFilterConfiguration,
+} from "@robbot/robbot-core/dist/middleware/logging"
 
-const apiToken = process.env.DISCORD_API_TOKEN as string
+const apiToken = process.env.DISCORD_API_TOKEN
+
+if (typeof apiToken !== "string") {
+  console.error(
+    `Error, environment variable <DISCORD_API_TOKEN> is not correctly set! Current value is <${apiToken}>`
+  )
+  process.exit(1)
+}
 
 const configuration = createDefaultConfiguration(apiToken)
+const loggingConfiguration: LogLevelFilterConfiguration = {
+  logLevel: LogLevel.DEBUG,
+}
+
+configuration.middleware = {
+  loggingMiddleware: [createLoggingLevelFilter(loggingConfiguration)],
+}
+
 const robBot = createClient(configuration)
 
-robBot.run()
+robBot.run().catch(() => process.exit(1))
