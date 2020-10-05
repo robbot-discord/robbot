7f66c8c3201eb2e2c2943a88eb30b26a7a643236
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-10-05 02:10:21 -0400

    Simplify per-server-event-handlers

--- middleware/per-server-event-handlers/src/index.ts
+++ middleware/per-server-event-handlers/src/index.ts
@@ -5,47 +5,29 @@ import { EventHandlerMiddleware } from "@robbot/robbot-core"
 import { RobBotClient } from "@robbot/robbot-core"
 
 export const createServerFilterMiddleware = (
+  client: RobBotClient,
   configuration: PerServerEventHandlers
 ): EventHandlerMiddleware => {
-  // TODO is there a way to write this in ES6 () => {} syntax?
-  function newMiddleware(eventHandlers: EventHandlers): EventHandlers
-  function newMiddleware(
+  const newMiddleware = (
     eventHandlersCreator: EventHandlersCreator
-  ): EventHandlersCreator
+  ): EventHandlersCreator => {
+    const eventHandlers = eventHandlersCreator(client)
+    const newEventHandlers = produce(eventHandlers, (draft) => {
+      draft.message = (message) => {
+        if (message.guild) {
+          const guildSnowflake = message.guild.id
+          const guildMessageHandler = configuration[guildSnowflake]?.message
 
-  function newMiddleware(eventHandlersOrCreator: unknown): unknown {
-    const isFunction = typeof eventHandlersOrCreator === "function"
-    const isObject = typeof eventHandlersOrCreator === "object"
-
-    if (isFunction) {
-      const eventCreator = (eventHandlersOrCreator as unknown) as EventHandlersCreator
-      const returnValue = (client: RobBotClient) => {
-        return eventCreator(client)
-      }
-
-      return returnValue
-    } else if (isObject) {
-      const eventHandlers = (eventHandlersOrCreator as unknown) as EventHandlers
-      const newEventHandlers = produce(eventHandlers, (draft) => {
-        draft.message = (message) => {
-          if (message.guild) {
-            const guildSnowflake = message.guild.id
-            const guildMessageHandler = configuration[guildSnowflake]?.message
-
-            if (guildMessageHandler) {
-              guildMessageHandler(message)
-            } else {
-              eventHandlers.message(message)
-            }
+          if (guildMessageHandler) {
+            guildMessageHandler(message)
+          } else {
+            eventHandlers.message(message)
           }
         }
-      })
+      }
+    })
 
-      return newEventHandlers
-    }
-
-    // in case of incorrect argument type, no-op
-    return eventHandlersOrCreator
+    return () => newEventHandlers
   }
 
   return newMiddleware
