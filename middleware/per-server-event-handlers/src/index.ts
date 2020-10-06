e97edd1e8575e4281c6ec24b6bcae2386f9fe095
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-10-06 00:25:22 -0400

    Simplify immer draft code

--- middleware/per-server-event-handlers/src/index.ts
+++ middleware/per-server-event-handlers/src/index.ts
@@ -1,8 +1,9 @@
 import { PerServerEventHandlers } from "./types"
-import { EventHandlersCreator, EventHandlers } from "@robbot/robbot-core"
+import { EventHandlersCreator } from "@robbot/robbot-core"
 import { produce } from "immer"
 import { EventHandlerMiddleware } from "@robbot/robbot-core"
 import { RobBotClient } from "@robbot/robbot-core"
+import { Message } from "discord.js"
 
 export const createServerFilterMiddleware = (
   client: RobBotClient,
@@ -11,21 +12,26 @@ export const createServerFilterMiddleware = (
   const newMiddleware = (eventHandlersCreator: EventHandlersCreator): EventHandlersCreator => {
     const eventHandlers = eventHandlersCreator(client)
     const logger = client.configuration.logger
-    const newEventHandlers = produce(eventHandlers, (draft) => {
-      draft.message = (message) => {
-        if (message.guild) {
-          const guildSnowflake = message.guild.id
-          const guildMessageHandler = configuration[guildSnowflake]?.message
 
-          if (guildMessageHandler) {
-            logger.debug(`Found guildMessageHandler for guild.id <${guildSnowflake}>`)
-            guildMessageHandler(message)
-          } else {
-            logger.debug(`No guildMessageHandler found for guild.id <${guildSnowflake}>, using default`)
-            eventHandlers.message(message)
-          }
-        }
+    const newMessageHandler = (message: Message) => {
+      if (!message.guild) {
+        return
       }
+
+      const guildSnowflake = message.guild.id
+      const guildMessageHandler = configuration[guildSnowflake]?.message
+
+      if (guildMessageHandler) {
+        logger.debug(`Found guildMessageHandler for guild.id <${guildSnowflake}>`)
+        guildMessageHandler(message)
+      } else {
+        logger.debug(`No guildMessageHandler found for guild.id <${guildSnowflake}>, using default`)
+        eventHandlers.message(message)
+      }
+    }
+
+    const newEventHandlers = produce(eventHandlers, (draft) => {
+      draft.message = newMessageHandler
     })
 
     return () => newEventHandlers
