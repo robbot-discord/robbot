08965ae4e302add1f3411018a2324258ec223c0e
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-10-06 01:38:09 -0400

    Fix serverFilterMiddleware to avoid client arg

--- middleware/per-server-event-handlers/src/index.ts
+++ middleware/per-server-event-handlers/src/index.ts
@@ -5,36 +5,35 @@ import { EventHandlerMiddleware } from "@robbot/robbot-core"
 import { RobBotClient } from "@robbot/robbot-core"
 import { Message } from "discord.js"
 
-export const createServerFilterMiddleware = (
-  client: RobBotClient,
-  configuration: PerServerEventHandlers
-): EventHandlerMiddleware => {
+export const createServerFilterMiddleware = (configuration: PerServerEventHandlers): EventHandlerMiddleware => {
   const newMiddleware = (eventHandlersCreator: EventHandlersCreator): EventHandlersCreator => {
-    const eventHandlers = eventHandlersCreator(client)
-    const logger = client.configuration.logger
+    return (client: RobBotClient) => {
+      const eventHandlers = eventHandlersCreator(client)
+      const logger = client.configuration.logger
 
-    const newMessageHandler = (message: Message) => {
-      if (!message.guild) {
-        return
+      const newMessageHandler = (message: Message) => {
+        if (!message.guild) {
+          return
+        }
+
+        const guildSnowflake = message.guild.id
+        const guildMessageHandler = configuration[guildSnowflake]?.message
+
+        if (guildMessageHandler) {
+          logger.debug(`Found guildMessageHandler for guild.id <${guildSnowflake}>`)
+          guildMessageHandler(message)
+        } else {
+          logger.debug(`No guildMessageHandler found for guild.id <${guildSnowflake}>, using default`)
+          eventHandlers.message(message)
+        }
       }
 
-      const guildSnowflake = message.guild.id
-      const guildMessageHandler = configuration[guildSnowflake]?.message
+      const newEventHandlers = produce(eventHandlers, (draft) => {
+        draft.message = newMessageHandler
+      })
 
-      if (guildMessageHandler) {
-        logger.debug(`Found guildMessageHandler for guild.id <${guildSnowflake}>`)
-        guildMessageHandler(message)
-      } else {
-        logger.debug(`No guildMessageHandler found for guild.id <${guildSnowflake}>, using default`)
-        eventHandlers.message(message)
-      }
+      return newEventHandlers
     }
-
-    const newEventHandlers = produce(eventHandlers, (draft) => {
-      draft.message = newMessageHandler
-    })
-
-    return () => newEventHandlers
   }
 
   return newMiddleware
